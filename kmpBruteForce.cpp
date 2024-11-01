ebdb96d4665f73bdf548f3539d86534b71ea9ddd
Abdullah Shoab <imshoab883@gmail.com>
2024-11-01 12:32:28 +0600

    Add files via upload

---
+++ kmpBruteForce.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int kmp(string str1, string str2){
+	int n=str1.length(),m=str2.length();
+
+	for(int i=0; i<n-m+1; i++){
+		int j=0;
+
+		while(j<m && str1[i+j]==str2[j]){
+			j++;
+		}
+
+		if(j==m){
+			return i;
+		}
+	}
+	return -1;
+}
+int main(){
+	string str1,str2;
+	cin>>str1>>str2;
+
+	int index = kmp(str1,str2);
+
+	if(index==-1){
+		cout<<"Not Found"<<endl;
+	}else{
+		cout<<"Found at"<<" "<<index<<endl;
+	}
+
+	return 0;
+}
